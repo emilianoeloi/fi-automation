0c967676ea6b50c4bc3f6172ff900c279059c914
emiliano.barbosa <emiliano.barbosa@corp.globo.com>
2016-02-18 08:58:47 -0200

    CHANGE: List Portfolio

--- be/App.js
+++ be/App.js
@@ -1,45 +1,83 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
 
 	constructor(){
 		super();
 		this.state = {
-			input: '/* add your jsx here */',
-			output: '',
-			err: ''
+			data: [],
+			odd: true
 		}
-		this.update = this.update.bind(this);
 	}
 
-	update(e){
-		let code = e.target.value;
-		try{
-			this.setState({
-				output: babel.transform(code, {
-					stage: 0,
-					loose: 'all'
-				}).code
-			})
-		}catch(err){
-			this.setState({err: err.message});
-		}
+	componentDidMount(){
+		console.info('componentDidMount');
+		$.ajax({
+			url:"http://emiliano.bocamuchas.org:5000/api/1.0/stock",
+			dataType: 'json',
+			cache: false,
+			success: function(data) {
+				console.info('componentDidMount','success', data);
+				this.setState({data:data, odd:true});
+			}.bind(this),
+			error: function(xhr, status, err){
+				console.info('componentDidMount', 'error', err);
+				console.error(this.props.url, status, err.toString());
+			}.bind(this)
+		});
 	}
 
 	render(){
+		return (<StockTable data={this.state.data} />)
+	}
+}
+
+class StockTable extends React.Component {
+	render(){
+		let rows = this.props.data.map( (stock,i) => {
+			return <StockRow data={stock} key={stock._id} index={i} />
+		})
 		return (
-			<div>
-				<header>
-					{this.state.err}
-				</header>
-				<div className="container">
-					<textarea onChange={this.update}
-					defaultValue={this.state.input}></textarea>
-					<pre>{this.state.output}</pre>
-				</div>
-			</div>
+			<table className="pure-table">
+			    <thead>
+			        <tr>
+			            <th>Stock</th>
+			            <th>Total</th>
+			            <th>Price</th>
+			            <th>Var</th>
+			        </tr>
+			    </thead>
+
+			    
+			        {rows}
+			    
+			</table>
 		)
 	}
+}	
+
+class StockRow extends React.Component {
+
+	render(){
+		console.log(this.props.index);
+		let odd = (this.props.index % 2) ? "pure-table-odd": "";
+		return 	<tbody>
+				<tr className={odd}>
+					<td>{this.props.data.code}</td>
+					<td>qtd: {this.props.data.qtd}</td>
+					<td>current: {this.props.data.current}</td>
+					<td>{this.props.data.variation}</td>
+				</tr>
+				<tr className={odd}>
+					<td>{this.props.data.name}</td>
+					<td>total: {this.props.data.total}</td>
+					<td>medium: {this.props.data.medium}</td>
+					<td>{this.props.data.rate}%</td>
+				</tr>
+		   	</tbody>	
+	}
+
 }
 
 export default App
